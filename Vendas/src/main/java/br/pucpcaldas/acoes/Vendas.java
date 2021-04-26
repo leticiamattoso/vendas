47f6915a8c07c708ec4210dc7f559323bd2ecba0
Marcelo Assis <marceloassis50@gmail.com>
2021-04-26 14:44:17 -0300

    Inseri as estorias de usuario no readme

--- Vendas/src/main/java/br/pucpcaldas/acoes/Vendas.java
+++ Vendas/src/main/java/br/pucpcaldas/acoes/Vendas.java
@@ -92,5 +92,5 @@ public class Vendas {
         return data;
     }
 
-    public Vendas(){}
+    
 }
--- Vendas/src/test/java/br/pucpcaldas/acoes/dominio/VendasBuilderTest.java
+++ Vendas/src/test/java/br/pucpcaldas/acoes/dominio/VendasBuilderTest.java
@@ -1,5 +1,19 @@
 package br.pucpcaldas.acoes.dominio;
 
+
+import static org.junit.Assert.assertTrue;
+
+import org.joda.time.LocalDate;
+import org.junit.Ignore;
+import org.junit.Test;
+
 public class VendasBuilderTest {
     
+    @Test
+    public void deveRetornarUmaCompra(){
+        VendasBuilder builder = new VendasBuilder();
+
+        Vendas umaVenda = builder.produto("Notebook Dell").preco(2000).quantidade(2).precoTotal(4000)
+        .data(LocalDate.now()).build();
+    }
 }
